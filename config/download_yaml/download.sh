544a9c9bf74bf8afa13d25d57b3614e25494cb59
wyt <80101277@qq.com>
2023-01-06 14:52:38 +0800

    init version

---
+++ config/download_yaml/download.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/lima-vm/lima/master/examples/debian.yaml -o debian.yaml
+curl https://raw.githubusercontent.com/lima-vm/lima/master/examples/default.yaml -o default.yaml
+curl https://raw.githubusercontent.com/lima-vm/lima/master/examples/docker.yaml -o docker.yaml
